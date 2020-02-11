afda57dae965654e55d0eadb35eb6ed2ab5b861c
Darren Harroff <darren.harroff@gmail.com>
2020-02-11 13:13:34 -0600

    updated css and js for cdn link

--- js/animation.js
+++ js/animation.js
@@ -114,39 +114,3 @@ overlay.addEventListener("click", e => {
     }, 250);
   }
 });
-
-const els = document.querySelectorAll("[data-aos]");
-const options = {
-  rootMargin: "-15%"
-};
-els.forEach(el => {
-  if (el.getAttribute("data-delay")) {
-    el.style.animationDelay = el.getAttribute("data-delay") + "ms";
-  }
-  const aosObs = new IntersectionObserver((entries, aosObs) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        el.classList.add("anim");
-        aosObs.unobserve;
-      }
-    });
-  }, options);
-  aosObs.observe(el);
-});
-
-const images = document.querySelectorAll(".lazy");
-const lazyOptions = {
-  rootMargin: "40% 0%"
-};
-images.forEach(image => {
-  const imageObs = new IntersectionObserver((entries, imageObs) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        image.src = image.getAttribute("data-src");
-        image.classList.add("lazy-in");
-        imageObs.unobserve;
-      }
-    });
-  }, lazyOptions);
-  imageObs.observe(image);
-});
